d64a44798a168ff4457ad522faa295f78f54032e
dahvde <owlsensation@gmail.com>
2021-07-11 11:14:52 -0700

    - Fixed copy on click

--- public/scripts/copy.js
+++ public/scripts/copy.js
@@ -1,13 +1,16 @@
 const blacklistedRefs = ["Click and Register", "N/A"];
 
-setTimeout(() => {
-    document.querySelectorAll(".cellRef").forEach((e) => {
-        e.addEventListener("click", (e) => {
-            const target = e.target;
-            if (blacklistedRefs.indexOf(target.innerHTML) === -1) {
-                target.select();
-                document.execCommand("copy");
-            }
+$(() => {
+    setTimeout(() => {
+        document.querySelectorAll(".cellRef").forEach((e) => {
+            e.addEventListener("click", (e) => {
+                const target = e.target;
+                if (blacklistedRefs.indexOf(target.innerHTML) === -1) {
+                    target.select();
+                    document.execCommand("copy");
+                }
+            });
+            console.log("worked");
         });
-    });
-}, 1000);
+    }, 700);
+});
